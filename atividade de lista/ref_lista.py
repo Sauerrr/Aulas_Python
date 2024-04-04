05d9524378937ec85824eaa405977097ba50fce0
Sauerrr <128698531+Sauerrr@users.noreply.github.com>
2024-04-04 10:38:09 -0300

    Add files via upload

--- atividade de lista/ref_lista.py
+++ atividade de lista/ref_lista.py
@@ -1,8 +1,12 @@
+import os
+
+
 def mostrarLista(lista):
     print("Sua lista atual é essa: ", lista)
 
 def gravarLista(lista):
     nome_arq = input("Digite o nome do arquivo:")
+    nome_arq += ".txt"
     with open (nome_arq, "w") as arquivo:
         for item in lista:
             arquivo.write(item + "/n")
@@ -10,6 +14,7 @@ def gravarLista(lista):
 
 def carregar_arquivo(lista):
     nome_arquivo = input("Digite o nome do arquivo para carregar a lista:")
+    nome_arquivo += ".txt"
     try:
         with open (nome_arquivo, "r") as arquivo:
             lista.clear()
@@ -26,7 +31,14 @@ def ordenar_lista(lista):
     lista.sort(reversed = True)
     print("Lista ordenada com sucesso")
 
+def listar_arquivos(extensao = ".txt"):
 
+    diretorios = os.getcwd()
+    arquivos = os.listdir(diretorios)
+    print(f"Os arquivos .{extensao} desse diretorio são:")
+    for lista_arquivo in arquivos:
+        if lista_arquivo.endswith(extensao):
+            print(lista_arquivo)
 
 
 def adicionarItem(lista):
@@ -52,9 +64,10 @@ def main():
         print("2 - Excluir item")
         print("3 - Exibir lista")
         print("4 - Gravar lista")
-        print("5 - Carregar o arquivo")
-        print("6 - Ordenar lista")
-        print("7 - Sair")
+        print("5 - Listar arquivos")
+        print("6 - Carregar o arquivo")
+        print("7 - Ordenar lista")
+        print("8 - Sair")
 
 
         opcao = input("Digite o número da opção que voce deseja: ")
@@ -68,15 +81,16 @@ def main():
         elif opcao == "4":
             gravarLista(lista)
         elif opcao == "5":
-            carregar_arquivo(lista)
+            listar_arquivos()
         elif opcao == "6":
-            ordenar_lista(lista)
+            carregar_arquivo(lista)
         elif opcao == "7":
+            ordenar_lista(lista)
+        elif opcao == "8":
             continuar = False
         else:
             print("Opção inválida. Digite novamente.")
 
-        print()
 
     print("Fim da lista.")
 
