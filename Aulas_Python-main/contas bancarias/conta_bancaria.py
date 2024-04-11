e529b87b4cc8b730bb708419b2f8dbfa1c3dd40b
Sauerrr <128698531+Sauerrr@users.noreply.github.com>
2024-04-11 08:14:27 -0300

    Delete Aulas_Python-main directory

--- Aulas_Python-main/contas bancarias/conta_bancaria.py
+++
@@ -1,8 +0,0 @@
-class ContaBancaria: 
-    def __init__(self, numero, titular, saldo = 0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-    
-    def deposita(self,valor):
-        self.saldo += valor
--- Aulas_Python-main/main.py
+++
@@ -1,7 +0,0 @@
-nome = input("Escreva seu nome, meu querido!");
-try: 
-    idade = int(input("Escreva sua idade, meu querido!"));
-except ValueError:
-    print("Voce é buro e digitou errado")
-print(f"Ola{nome},você tem {idade} anos");
- 
